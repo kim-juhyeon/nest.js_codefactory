469f20fe7d93e9d17e262c4981da475674d07cfa
jju <totkfka4@gmail.com>
2024-03-08 00:04:38 +0900

    로그인 로직 만드는중 v2

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UsersModel } from 'src/users/entities/users.entity';
-import { JWT_SECRET } from './const/auth.const';
+import { HASH_ROUNDS, JWT_SECRET } from './const/auth.const';
 import { UsersService } from 'src/users/users.service';
 import * as bcrypt from 'bcrypt';
 
@@ -10,7 +10,35 @@ export class AuthService {
     constructor(
         private readonly jwtService: JwtService,
         private readonly usersService: UsersService,
-    ){}
+    ) { }
+    /**
+     * 토큰을 사용하게 되는 방식
+     * 
+     * 1) 사용자가 로그인 또는 회원가입을 진행하면
+     *      accessToken과 refreshToken을 발급받는다.
+     * 2) 로그인 할때는 Basic 토큰과 함께 요청을 보낸다.
+     *      Basic 토큰은 '이메일:비밀번호'를 Base64로 인코딩한 형태입니다.
+     *      예) {authorization: 'Basic {token}'}
+     * 3) 아무나 접근 할 수 없는 정보 (private route)를 접근할때는
+     *      accessToken을 Header에 추가해서 요청과 함께 보낸다.
+     *      예) {authorization: 'Brearer {token}'}
+     * 
+     *  4) 토큰과 요청을 함께 받은 서버는 토큰 검증을 통해 현재 요청을 보낸 사용자가 누구인지 알 수 있다.
+     *      예를들어서 현재 로그인한 사용자가 작성한 포스트만 가져오려면 
+     *      토큰의 sub값에 입력돼있는 사용자의 포스트만 따로 필터링 할 수 있다.
+     *      특정 사용자의 토큰이 없다면 다른 사용자의 데이터를 접근 못한다.
+     * 
+     *  5) 모든 토큰은 만료 기간이 있다. 만료기간이 지나면, 새로 토큰을 받급받아야 한다.
+     *      그렇지 않으면, jwtService.verify()에서 인증이 통과가 안된다.
+     *      그러니 access토큰을 새로 발급 받을 수 있는 /auth/token/access와
+     *      refresh토큰을 새로 발급 받을 수 있는 /auth/token/refresh가 필요하다.
+     * 
+     *  6) 토큰이 만료되면, 각각의 토큰을 새로 발급 받을 수 있는 엔드포인트에 요청을 해서
+     *      새로운 토큰을 발급받고 새로운 토큰을 사용해서 private route에 접근한다.
+     */
+
+
+
     /**
      * 만들려고 하는기능
      * 1)  registerWithEmail
@@ -85,5 +113,18 @@ export class AuthService {
         const existingUser = await this.authenticateWithEmailAndPassword(user);
 
         return this.loginUser(existingUser);
+    }
+
+    async registerWithEmail(user: Pick<UsersModel, 'nickname' | 'email' | 'password'>) {
+        const hash = await bcrypt.hash(
+            user.password, //hash.라운드가 커질 수록 오래걸림 = 서버가 많아짐
+            HASH_ROUNDS,
+        );
+        const newUser = await this.usersService.createUser({
+            ...user,
+            password: hash,
+        });
+
+        return this.loginUser(newUser);
     }
 }
