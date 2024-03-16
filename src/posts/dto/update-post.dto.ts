cdfba094db5c7129960d1ff95bf7bbfaef7c9bd9
jju <totkfka4@gmail.com>
2024-03-16 23:46:41 +0900

    postpost

---
+++ src/posts/dto/update-post.dto.ts
@@ -0,0 +1,14 @@
+import { PartialType, PickType } from "@nestjs/mapped-types";
+import { createPostDto } from "./create-post.dto";
+import { IsOptional, IsString } from "class-validator";
+
+export class UpdatePostDto extends PartialType(createPostDto){
+    @IsString()
+        @IsOptional()
+    title?: string;
+    
+    @IsString()
+    @IsOptional()
+    content?: string;
+
+}
